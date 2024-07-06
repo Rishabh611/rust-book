f8c74a39d80d155ec37fc9fcde586413e6ef972a
Rishabh <rishabsinghrock@gmail.com>
2024-07-06 16:36:15 +0530

    tests

---
+++ CH-11/adder/src/lib.rs
@@ -0,0 +1,22 @@
+pub fn add_two(a: i32) -> i32 {
+    a + 2
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_adds_two(){
+        assert_eq!(4, add_two(2));
+    }
+    
+    #[test]
+    fn it_works()->Result<(), String>{
+        if 2+2 == 4 {
+            Ok(())
+        } else {
+            Err(String::from("two plus two does not equal four"))
+        }
+    }
+}
